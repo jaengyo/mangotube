100d858aa0d6c17cb41dcaf9fad81500beb7fcc0
jaengyo <74586570+jaengyo@users.noreply.github.com>
2021-01-21 20:03:34 +0900

    Update middle ware

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+// var express = require("express");
+import "core-js";
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`listening on http://localhost:${PORT}`);
+};
+
+const handleHome = (req, res) => {
+  res.send("hello it is response");
+};
+
+const handelProfile = (req, res) => {
+  res.send("you are on my profile");
+};
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true})
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.get("/", handleHome);
+app.get("/profile", handelProfile);
+app.listen(4000, handleListening);
